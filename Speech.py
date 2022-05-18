25be2c960836786902a2451d38004cba4d1ede1e
marghovo <marg.hovo@gmail.com>
2022-05-18 19:11:03 +0400

    Add get summary to Speech

--- Speech.py
+++ Speech.py
@@ -6,7 +6,7 @@ class Speech:
     def __init__(self, content: str):
         self.content = content
 
-    def get_emotion_scores(self, model: str = "j-hartmann/emotion-english-distilroberta-base"):
+    def get_emotion_scores(self, model: str = "j-hartmann/emotion-english-distilroberta-base") -> dict:
         """
 
         :param model: model name from Huggingface
@@ -15,7 +15,7 @@ class Speech:
         classifier = pipeline("text-classification", model=model, return_all_scores=True)
         return classifier(self.content, truncation=True)
 
-    def get_emotion_scores_by_sentence(self, model: str = "j-hartmann/emotion-english-distilroberta-base"):
+    def get_emotion_scores_by_sentence(self, model: str = "j-hartmann/emotion-english-distilroberta-base") -> dict:
         """
 
         :param model: model name from Huggingface
@@ -28,3 +28,19 @@ class Speech:
             emotion_scores[i] = classifier(str(sentence), truncation=True)
 
         return emotion_scores
+
+    def get_summary(self, model: str = "facebook/bart-large-cnn", max_length: int = 256, inplace: bool = False) -> str:
+        """
+
+        :param model: model name from Huggingface
+        :param max_length: max length of the summary
+        :param inplace: if true self.content is replaced by summary
+        :return:
+        """
+        summarizer = pipeline("summarization", model=model)
+        summary = summarizer(self.content, truncation=True, max_length=max_length)[0]["summary_text"]
+        if inplace:
+            self.content = summary
+            return self.content
+        else:
+            return summary
